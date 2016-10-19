41189ce8ef1e65ba6f56862e874aea35343c24b7
Balazs Mocsai <me@bael.me>
2016-10-19 10:23:46 +0200

    fixed trailing slash issue
    Signed-off-by: Balazs Mocsai <me@bael.me>

--- frontend/client/app/components/detail/detail.js
+++ frontend/client/app/components/detail/detail.js
@@ -6,13 +6,23 @@ const detailModule = angular.module('detail', [
   uiRouter,
 ])
 
-.config(($stateProvider) => {
+.config(($stateProvider, $urlMatcherFactoryProvider) => {
   'ngInject';
+
+  // make ending trailing slash irrelevant on routing
+  $urlMatcherFactoryProvider.strictMode(false);
 
   $stateProvider
     .state('detail', {
       url: '/{username}/{pointdogName}',
       component: 'detail',
+      params: {
+        // setup default pointdogname if null
+        pointdogName: {
+          value: 'default',
+          squash: true,
+        },
+      },
     });
 })
 
