ab9723e0f54eea979971a6ff082a4be971401ae1
Balazs Mocsai <me@bael.me>
2016-10-19 11:38:42 +0200

    firebase refactor to app module hook
    Signed-off-by: Balazs Mocsai <me@bael.me>

--- frontend/client/app/app.controller.js
+++ frontend/client/app/app.controller.js
@@ -1,16 +1,6 @@
 class AppController {
   constructor() {
     'ngInject';
-
-    const config = {
-      apiKey: 'AIzaSyBDdWtz7vzIJCVs9zOxJAkPLUAy6UV2r2w',
-      authDomain: 'pointdog-ba7c4.firebaseapp.com',
-      databaseURL: 'https://pointdog-ba7c4.firebaseio.com',
-      storageBucket: 'pointdog-ba7c4.appspot.com',
-      messagingSenderId: '719778653133',
-    };
-
-    firebase.initializeApp(config);
   }
 }
 
--- frontend/client/app/app.js
+++ frontend/client/app/app.js
@@ -27,6 +27,16 @@ angular.module('app', [
   $urlRouterProvider.when('', '/');
   // set 404 router to default
   $urlRouterProvider.otherwise('/');
+
+  const config = {
+    apiKey: 'AIzaSyBDdWtz7vzIJCVs9zOxJAkPLUAy6UV2r2w',
+    authDomain: 'pointdog-ba7c4.firebaseapp.com',
+    databaseURL: 'https://pointdog-ba7c4.firebaseio.com',
+    storageBucket: 'pointdog-ba7c4.appspot.com',
+    messagingSenderId: '719778653133',
+  };
+
+  firebase.initializeApp(config);
 })
 
 .run(($transitions, ngMeta) => {
