ad41051fe129dc2a44f7cf553d01a82cd888b34b
Balazs Mocsai <me@bael.me>
2016-10-20 13:18:11 +0200

    404 logics for pointdog / user
    Signed-off-by: Balazs Mocsai <me@bael.me>

--- frontend/client/app/components/detail/detail.controller.js
+++ frontend/client/app/components/detail/detail.controller.js
@@ -10,7 +10,16 @@ class DetailController {
     this.isAuthenticated = AuthService.authenticated;
     const dataToAuthenticate = AuthService.dataToAuthenticate;
 
+    // carousel
     this.images = [];
+    // map urls
+    this.mapUrls = {
+      google: '',
+      apple: '',
+      tomtom: '',
+      waze: '',
+      streetview: '',
+    };
 
     // data pipeline
     UserService
@@ -18,11 +27,50 @@ class DetailController {
       .getByName(username)
       // bind user & get pointdog
       .then((user) => {
+        // validate user
+        if (!user.uid) {
+          return Promise.reject({
+            code: 0,
+            data: user,
+          });
+        }
         this.user = user;
 
         console.log(user);
         // return pointdog promise
         return PointdogService.getByUId(user.uid, pointdogName);
+      })
+      // set map urls
+      .then((pointdog) => {
+        // validating pointdog
+        if (!pointdog.key) {
+          return Promise.reject({
+            code: 1,
+            data: pointdog,
+          });
+        }
+        console.log(pointdog)
+        // set default (desktop) URL-s
+        const {
+          lat,
+          lng,
+        } = pointdog.location;
+
+        const latitude = window._.round(lat, 6);
+        const longitude = window._.round(lng, 6);
+
+        this.mapUrls.google = `http://maps.google.com/maps?q=${latitude},${longitude}`;
+        this.mapUrls.apple = `http://maps.apple.com/?q=${latitude},${longitude}`;
+        this.mapUrls.waze = `https://www.waze.com/livemap?zoom=14&lat=${latitude}&lon=${longitude}`;
+        this.mapUrls.streetview = `https://www.instantstreetview.com/@${latitude},${longitude},18z,1t`;
+
+        // check urls & set based on navigator
+        if (window.navigator.userAgent.match(/iPhone|iPod/)) {
+          this.mapUrls.waze = `waze://?ll=${pointdog.location.lat},${pointdog.location.lng}&t=m`;
+          this.mapUrls.streetview = `comgooglemaps://?center=${pointdog.location.lat},${pointdog.location.lng}&mapmode=streetview`;
+        }
+
+        return pointdog;
       })
       // bind pointdog & get images
       .then((pointdog) => {
@@ -59,7 +107,20 @@ class DetailController {
           this.images.push(imageUrls[i]);
         }
       })
-      // authenticate
+      .then(() => {
+        console.log('carouselfix')
+        function carouselNormalization() {
+          const items = $('.item'); //grab all slides
+          const heights = []; //create empty array to store height values
+          let tallest; //create variable to make note of the tallest slide
+
+          console.log(items)
+          console.log(document.getElementsByClassName('item'))
+        }
+
+        carouselNormalization();
+      })
+      // authenticate validiation
       .then(() => {
         if (this.user.isPrivate) {
           if (!this.isAuthenticated) {
@@ -74,11 +135,22 @@ class DetailController {
         // set it true - messy way
         this.isAuthenticated = true;
       })
-      // if user/pointdog is absent or db error occured
+      // data validation / db error
       .catch((error) => {
-        // todo check what kind of error
-        console.log(error);
-        //$state.go('index');
+        switch (error.code) {
+        case 0:
+          console.log('no user')
+          $state.go('notfound');
+          break;
+        case 1:
+          console.log('no pointdog');
+          $state.go('notfound');
+          break;
+        default:
+          // unhandled
+          console.log(error)
+          $state.go('index');
+        }
       });
   }
 }
