b227f9f0354a6307a4edacee48295ebb96e0bb9d
Balazs Mocsai <me@bael.me>
2016-10-20 13:17:45 +0200

    added 404 component
    Signed-off-by: Balazs Mocsai <me@bael.me>

--- frontend/client/app/components/components.js
+++ frontend/client/app/components/components.js
@@ -2,11 +2,13 @@ import angular from 'angular';
 import Detail from './detail/detail';
 import Index from './index/index';
 import Auth from './auth/auth';
+import NotFound from './notfound/notfound'
 
 const componentModule = angular.module('app.components', [
   Detail,
   Index,
   Auth,
+  NotFound,
 ])
 
 .name;
---
+++ frontend/client/app/components/notfound/notfound.component.js
@@ -0,0 +1,12 @@
+import template from './notfound.html';
+import controller from './notfound.controller';
+import './notfound.styl';
+
+const notFoundComponent = {
+  restrict: 'E',
+  bindings: {},
+  template,
+  controller,
+};
+
+export default notFoundComponent;
